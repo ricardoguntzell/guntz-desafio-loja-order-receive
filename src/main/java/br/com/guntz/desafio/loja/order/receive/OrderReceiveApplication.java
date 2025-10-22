5c31bb3157f721f1787ffeb611d9eb9d43cd059d
guntz <rricardoguntzell@gmail.com>
2025-10-22 14:08:35 -0300

    feat: Inicializando projeto Order Receive

---
+++ src/main/java/br/com/guntz/desafio/loja/order/receive/OrderReceiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.guntz.desafio.loja.order.receive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderReceiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderReceiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guntz/desafio/loja/order/receive/OrderReceiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guntz.desafio.loja.order.receive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderReceiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
